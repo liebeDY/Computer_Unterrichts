4112fcc611f77c2e7f8f6b4fade2f044f0c00eea
liebeDY <hdydeveloper0927@gmail.com>
2020-11-12 22:15:04 +0900

    수업#1 2.파일 [boolean, 정수타입 예제]

---
+++ unterrichts/src/tag_1_step1/VarType.java
@@ -0,0 +1,19 @@
+package tag_1_step1;
+
+public class VarType {
+
+	public static void main(String[] args) {
+		
+		// 논리 타입
+		boolean a = true;
+		
+		// 정수 타입
+		byte b = 10;
+		short c = 20;
+		int d = 30;
+		long e = 40;
+	
+	
+	}
+
+}
