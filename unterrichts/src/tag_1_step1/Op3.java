000439fe0b3c358cb3510e64b54edbf4681d180f
liebeDY <hdydeveloper0927@gmail.com>
2020-11-12 22:56:07 +0900

    수업#4 6.파일 Op3.java [비교연산자 예제_1]

---
+++ unterrichts/src/tag_1_step1/Op3.java
@@ -0,0 +1,15 @@
+package tag_1_step1;
+
+public class Op3 {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b = 20;
+		
+		System.out.println(a == b); // a와 b의 값이 같은지 물어보는 것 -> false
+		System.out.println(a != b); // a와 b의 값이 다른지 물어보는 것 -> true
+		
+		
+	}
+
+}
