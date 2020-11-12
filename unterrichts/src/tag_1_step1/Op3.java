935cbaa1e96dacbb41e6a8061b2095812e7431d2
liebeDY <hdydeveloper0927@gmail.com>
2020-11-12 22:58:19 +0900

    수업#5 6.파일 Op3.java [비교연산자 예제_2]

--- unterrichts/src/tag_1_step1/Op3.java
+++ unterrichts/src/tag_1_step1/Op3.java
@@ -9,7 +9,10 @@ public class Op3 {
 		System.out.println(a == b); // a와 b의 값이 같은지 물어보는 것 -> false
 		System.out.println(a != b); // a와 b의 값이 다른지 물어보는 것 -> true
 		
-		
+		System.out.println(a > b); // a가 b보다 크다 -> false
+		System.out.println(a >= b); // a가 b보다 크거나 같다 -> false
+		System.out.println(a < b); // a가 b보다 작다 -> true
+		System.out.println(a <= b); // a가 b보다 작거나 같다 -> true
 	}
 
 }
