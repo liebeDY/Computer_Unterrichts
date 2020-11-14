6ccf2103e372b6c8a0779d1d8b2850d4c544739a
liebeDY <hdydeveloper0927@gmail.com>
2020-11-14 17:48:09 +0900

    수업#1 6.파일 For3.java [for문 예제_3]

---
+++ unterrichts/src/tag_2_step1/For3.java
@@ -0,0 +1,24 @@
+package tag_2_step1;
+
+public class For3 {
+
+	public static void main(String[] args) {
+		
+		int i;
+		
+		for (i = 1; i <= 100; i++) {
+			// È¦¼ö 
+			if (i % 2 != 0) {
+				System.out.println(i + "\t");
+			}
+			
+			// 10ÀÇ ¹è¼ö
+			if (i % 10 == 0) {
+				System.out.println();
+			}
+			
+			
+		}
+	}
+
+}
