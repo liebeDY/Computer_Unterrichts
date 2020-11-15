aa6dba25fd7340cfdfd681779d7afd69304ed253
liebeDY <hdydeveloper0927@gmail.com>
2020-11-15 17:08:34 +0900

    수업#1 7.파일 For4.java [for문 예제_6]

--- unterrichts/src/tag_2_step1/For4.java
+++ unterrichts/src/tag_2_step1/For4.java
@@ -9,8 +9,13 @@ public class For4 {
 //			System.out.println(i);
 //		}
 
-		for (int i = 1; i <= 10; i += 2) {
-
+//		for (int i = 1; i <= 10; i += 2) {
+//
+//			System.out.println(i);
+//		}
+		
+		for (int i = 2; i <= 10; i+=2) {
+			
 			System.out.println(i);
 		}
 	}
