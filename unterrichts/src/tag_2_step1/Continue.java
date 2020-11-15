2d532c2c00831fbe9bd64908cbc2e7d1a516c1b6
liebeDY <hdydeveloper0927@gmail.com>
2020-11-15 20:15:35 +0900

    수업#6 23.파일 Continue.java [continue 예제]

---
+++ unterrichts/src/tag_2_step1/Continue.java
@@ -0,0 +1,19 @@
+package tag_2_step1;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		
+		int i;
+		
+		for(i = 0; i < 11; i++) {
+			
+			if (i % 2 == 0) {
+				continue;
+			}
+			System.out.println(i + "\t");
+			
+		}
+	}
+
+}
