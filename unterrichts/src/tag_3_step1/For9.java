8d7d2ae8631b2bc32bd4071229f3ea0fd6073fef
liebeDY <hdydeveloper0927@gmail.com>
2020-11-16 22:26:17 +0900

    수업#4 17.파일 For9.java [for 예제_9]

---
+++ unterrichts/src/tag_3_step1/For9.java
@@ -0,0 +1,20 @@
+package tag_3_step1;
+
+public class For9 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+
+		for (int i = 0; i < 10; i++) {
+
+			if (i % 2 == 0) {
+				continue;
+			}
+
+			sum += i;
+			System.out.print(i + "\t");
+		}
+		System.out.println(sum);
+	}
+
+}
