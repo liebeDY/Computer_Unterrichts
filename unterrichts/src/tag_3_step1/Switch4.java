708595328ded71f4c55d869bfbe15221051b33cb
liebeDY <hdydeveloper0927@gmail.com>
2020-11-16 21:54:46 +0900

    수업#2 8.파일 Switch4.java [switch 예제_4]

---
+++ unterrichts/src/tag_3_step1/Switch4.java
@@ -0,0 +1,41 @@
+package tag_3_step1;
+
+import java.util.Scanner;
+
+public class Switch4 {
+
+	public static void main(String[] args) {
+		int a;
+
+		System.out.println("1~9까지의 숫자를 입력하세요 :");
+
+		Scanner sc = new Scanner(System.in);
+
+		a = sc.nextInt();
+
+		switch (a) {
+
+//		case 1,3,5,7,9:
+//			System.out.println("홀수 입니다.");
+//			break;
+//		case 2,4,6,8:
+//			System.out.println("짝수 입니다.");
+//			break;
+
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 9:
+			System.out.println("홀수 입니다.");
+			break;
+		case 2:
+		case 4:
+		case 6:
+		case 8:
+			System.out.println("짝수 입니다.");
+
+		}
+	}
+
+}
