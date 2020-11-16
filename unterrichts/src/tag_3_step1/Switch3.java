6e77e5d8e5689b8b56c093dae4076d100562faf1
liebeDY <hdydeveloper0927@gmail.com>
2020-11-16 21:46:08 +0900

    수업#2 7.파일 Switch3.java [switch 예제_3]

---
+++ unterrichts/src/tag_3_step1/Switch3.java
@@ -0,0 +1,41 @@
+package tag_3_step1;
+
+public class Switch3 {
+
+	public static void main(String[] args) {
+		
+		int jumsu = 85, mok;
+		char grade;
+
+		mok = jumsu / 10;
+
+		switch (mok) {
+
+		case 10:
+
+		case 9:
+			grade = 'A';
+			break;
+
+		case 8:
+			grade = 'B';
+			break;
+
+		case 7:
+			grade = 'C';
+			break;
+
+		case 6:
+			grade = 'D';
+			break;
+
+		default:
+			grade = 'F';
+			break;
+
+		}
+		System.out.println(grade);
+
+	}
+
+}
