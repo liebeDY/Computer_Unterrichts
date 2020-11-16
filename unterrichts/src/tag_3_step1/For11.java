21078fc7826c0bf732192716e0942c08a64305a1
liebeDY <hdydeveloper0927@gmail.com>
2020-11-16 22:30:30 +0900

    수업#4 19.파일 For11.java [for 예제_11]

---
+++ unterrichts/src/tag_3_step1/For11.java
@@ -0,0 +1,19 @@
+package tag_3_step1;
+
+public class For11 {
+
+	public static void main(String[] args) {
+
+		for(int i = 0; i <= 6; i++) {
+			
+			System.out.println(i);
+			
+			if (i == 1 || i == 3 || i == 5 || i == 7) {
+				System.out.println("È¦¼ö ÀÔ´Ï´Ù.");
+			} else if (i == 2 || i == 4 || i == 6) {
+				System.out.println("Â¦¼ö ÀÔ´Ï´Ù.");
+			}
+		}
+	}
+
+}
