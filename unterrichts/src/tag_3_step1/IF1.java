7fa35c75a66dcf6ca64d0b0f9ea74fc4145e95b6
liebeDY <hdydeveloper0927@gmail.com>
2020-11-16 21:31:44 +0900

    수업#1 1.파일 IF1.java [if 예제_1]

---
+++ unterrichts/src/tag_3_step1/IF1.java
@@ -0,0 +1,15 @@
+package tag_3_step1;
+
+public class IF1 {
+
+	public static void main(String[] args) {
+		boolean a = false;
+
+		if (a) {
+			System.out.println(10);
+		} else {
+			System.out.println(-10);
+		}
+	}
+
+}
