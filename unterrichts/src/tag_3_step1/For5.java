2585f9a1cd648bc81201f13dd7e456714ef6d8fe
liebeDY <hdydeveloper0927@gmail.com>
2020-11-16 22:17:42 +0900

    수업#3 13.파일 For5.java [for 예제_5]

---
+++ unterrichts/src/tag_3_step1/For5.java
@@ -0,0 +1,18 @@
+package tag_3_step1;
+
+public class For5 {
+
+	public static void main(String[] args) {
+		int i;
+		int sum = 0;
+		
+		for (i = 1; i <= 100; i++) {
+			
+			if (i % 5 != 0) { // 5의 배수가 아닐 때
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
