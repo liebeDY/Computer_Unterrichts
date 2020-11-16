6ce43691d3299483fc3e8d74708c43d71759d721
liebeDY <hdydeveloper0927@gmail.com>
2020-11-16 22:42:57 +0900

    수업#5 24.파일 Array2.java [array 예제_2]

---
+++ unterrichts/src/tag_3_step1/Array2.java
@@ -0,0 +1,14 @@
+package tag_3_step1;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		int[] a = new int[100];
+
+		for (int i = 0; i < 100; i++) {
+			a[i] = i;
+			System.out.println(a[i]);
+		}
+	}
+
+}
