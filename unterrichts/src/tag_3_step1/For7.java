fa26f085a8c14c32bbd4841e3ad42520eba50498
liebeDY <hdydeveloper0927@gmail.com>
2020-11-16 22:21:02 +0900

    수업#3 15.파일 For7.java [for 예제_7]

---
+++ unterrichts/src/tag_3_step1/For7.java
@@ -0,0 +1,16 @@
+package tag_3_step1;
+
+public class For7 {
+
+	public static void main(String[] args) {
+		int i;
+
+		for (i = 1; i <= 100; i++) {
+			System.out.print(i + " ");
+			if (i % 10 == 0) {
+				System.out.println();
+			}
+		}
+	}
+
+}
