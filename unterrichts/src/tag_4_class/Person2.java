6e3b1e2800b86f6b5cce8654d41c48ce69365c67
liebeDY <hdydeveloper0927@gmail.com>
2020-11-17 23:42:28 +0900

    수업#5 12.파일 Person2.java / PersonMain2.java [class 예제_6]

---
+++ unterrichts/src/tag_4_class/Person2.java
@@ -0,0 +1,11 @@
+package tag_4_class;
+
+public class Person2 {
+	int a;
+	int b;
+	
+	void print() {
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
---
+++ unterrichts/src/tag_4_class/PersonMain2.java
@@ -0,0 +1,13 @@
+package tag_4_class;
+
+public class PersonMain2 {
+
+	public static void main(String[] args) {
+		Person2 p = new Person2();
+
+		p.a = 1;
+		p.b = 2;
+
+		p.print();
+	}
+}
