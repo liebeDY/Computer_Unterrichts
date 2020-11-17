9843f3732bc1f69fe2e30016a170b636acdb9daa
liebeDY <hdydeveloper0927@gmail.com>
2020-11-17 23:10:04 +0900

    수업#3 7.파일 Person.java / PersonMain.java [class 예제_1]

---
+++ unterrichts/src/tag_4_class/Person.java
@@ -0,0 +1,6 @@
+package tag_4_class;
+
+public class Person {
+	int a;
+	int b;
+}
---
+++ unterrichts/src/tag_4_class/PersonMain.java
@@ -0,0 +1,14 @@
+package tag_4_class;
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+		
+		Person p = new Person(); // new -> 객체/인스턴스 생성, das deutet, dass adresse geben.
+
+		p.a = 1;
+		p.b = 2;
+		System.out.println(p.a);
+		System.out.println(p.b);
+	}
+}
